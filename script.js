450406122c97750c586f51c9d5af467c2abee448
GitZaidHub <mohzaid035@gmail.com>
2026-06-03 18:37:21 +0530

    updated Api

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let votes = {
 };
 
 let voters = [];
-const apiBaseUrl = "https://crudcrud.com/api/dfddbb5cfa134aef8faf4c0f3da5b8f4";
+const apiBaseUrl = "https://crudcrud.com/api/5a70f3da9ed84f6ba166533964d93a69";
 
 // UI Elements
 const voteBtn = document.getElementById("voteBtn");
